469be38a833e14fa914e32380b959a7eb10fea78
KISHAN KUMAR <kishaninn@gmail.com>
2025-02-18 20:23:13 +0530

    Add project files.

---
+++ SkyCast.WebSystem/Controllers/SkyCastController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SkyCast.WebSystem.Controllers;
+
+public class SkyCastController : Controller
+{
+    public IActionResult SkyCastIndex()
+    {
+        return View();
+    }
+}
